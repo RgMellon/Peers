7c08fa88bc63ccb206bae36d203ca4f2c6a100d8
Renan <renan_gian@hotmail.com>
2018-07-02 17:03:31 -0300

    alert

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,20 @@
 <template>
   <q-page padding class="docs-input row justify-center">
-      <div style="width: 500px; max-width: 90vw;" class="main">
-          <section class="busca">
-            <auto-complete @selecionou="mostraResultado" style="margin-top:2rem"></auto-complete>
-          </section>
-          <section class="lista-itens" v-if="this.mostra" style="margin-top:2rem" @click="redireciona">
-            <lista v-for="i of this.dados" :key="i.id"
-              :nome="i.nome" :descricao="i.descricao"
-              :img="i.img" :preco="i.preco">
-            </lista>
-          </section>
-          <section class="area-login row">
-             <q-btn color="primary" icon="fas fa-user" class="full-width" label="Login" />
-          </section>
-      </div>
+    <q-alert icon="fas fa-user" class="info" color="info">
+     Crie um perfil para melhor experiencia ;)
+    <router-link style="color:white;"to="/lojas"> Criar </router-link>
+    </q-alert>
+    <main style="width: 500px; max-width: 90vw; margin-top:2rem;">
+      <section class="busca">
+        <auto-complete @selecionou="mostraResultado" style="margin-top:2rem"></auto-complete>
+      </section>
+      <section class="lista-itens" v-if="this.mostra" style="margin-top:2rem" @click="redireciona">
+        <lista v-for="i of this.dados" :key="i.id"
+          :nome="i.nome" :descricao="i.descricao"
+          :img="i.img" :preco="i.preco">
+        </lista>
+      </section>
+    </main>
   </q-page>
 </template>
 
@@ -48,8 +49,11 @@ export default {
 }
 </script>
 
-<style>
-    .main {display: flex;
-    flex-direction: column;
-    justify-content: space-between;}
+<style scoped>
+  .info {
+    position:absolute;
+    top:1px;
+    right: 0;
+    left: 0;
+  }
 </style>
