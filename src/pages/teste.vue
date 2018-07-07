5e94d07e02f5d97de3d70b47bcc77eeed0af741f
Renan <renan_gian@hotmail.com>
2018-07-07 12:30:57 -0300

    add recorte img

--- src/pages/teste.vue
+++ src/pages/teste.vue
@@ -1,10 +1,13 @@
 <template>
-  <q-page>
-    <input type="file" name="image" accept="image/*"
-           style="font-size: 1.2em; padding: 10px 0;"
-           @change="setImage" />
+  <q-page padding>
+    <div style="font-size:60px; color:gray; margin-bottom:1rem; margin-top:-1rem;" class="cor-icone row justify-center">
+      <label for="file-input" class="flex">
+        <q-icon  name="fas fa-camera"/>
+      </label>
+      <input id="file-input" type="file" accept="image/*" @change="setImage"/>
+    </div>
 
-     <div style="width: 100%; height:300px; border: 1px solid gray; display: inline-block;">
+     <div style="width: 100%; height:300px; border: 1px solid gray; row justify-center">
       <vue-cropper
           ref='cropper'
           :guides="true"
@@ -17,12 +20,22 @@
           :rotatable="true"
           :src="imgSrc"
           alt="Source Image"
-          :img-style="{ 'width': '400px', 'height': '300px' }">
+          :img-style="{ 'width': '300px', 'height': '300px' }">
       </vue-cropper>
     </div>
-    <img :src="cropImg" style="width: 300px; height: 300px; border: 1px solid gray" alt="Cropped Image" />
-      <button @click="cropImage" v-if="imgSrc != ''" style="margin-right: 140px;">Crop</button>
-      <button @click="rotate" v-if="imgSrc != ''">Rotate</button>
+    <!-- area da img amostra e botoes -->
+    <div class="img-secao row justify-around items-center" style="margin-top:1rem">
+      <q-btn :rounded="true" color="amber" icon="fas fa-undo" style="height:50px;" @click="rotate" v-if="imgSrc != ''"/>
+
+      <img :src="cropImg" style="width: 100px;
+            height: 100px; border: 1px solid gray"
+            alt="img cortada" />
+
+      <q-btn :rounded="true" color="blue" style="height:50px;" icon="fas fa-cut" @click="cropImage" v-if="imgSrc != ''"
+      />
+    </div>
+    <q-btn :loading="load" color="primary" class="full-width" style="margin-top:2rem"
+    label="Salvar" />
   </q-page>
 </template>
 
@@ -41,7 +54,7 @@ export default {
       setImage(e) {
         const file = e.target.files[0];
         if (!file.type.includes('image/')) {
-          alert('Please select an image file');
+          alert('Por favor, selcione uma imagem.');
           return;
         }
         if (typeof FileReader === 'function') {
@@ -49,22 +62,22 @@ export default {
               .then(res => this.imgSrc =  res)
               .then(img => this.$refs.cropper.replace(img));
             }
-
-         else {
-          alert('Sorry, FileReader API not supported');
+        else {
+          alert('Desculpas, Errp');
         }
       },
       cropImage() {
-        // get image data for post processing, e.g. upload or setting image src
         this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL();
       },
       rotate() {
-        // guess what this does :)
         this.$refs.cropper.rotate(90);
       },
     },
   };
 </script>
 
-<style>
+<style scoped>
+  #file-input{
+    display: none;
+  }
 </style>
