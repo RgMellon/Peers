36022daa7292e81d1f7670b8831bcb71c26af980
Renan <renan_gian@hotmail.com>
2018-07-15 17:11:57 -0300

    testando galeria

---
+++ src/components/vueGaleria.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <gallery :images="images" :index="index" @close="index = null"></gallery>
+    <div
+      class="image"
+      v-for="(image, imageIndex) in images"
+      :key="imageIndex"
+      @click="index = imageIndex"
+      :style="{ backgroundImage: 'url(' + image + ')', width: '330px', height: '400px' }"
+    ></div>
+  </div>
+</template>
+
+<script>
+  import VueGallery from 'vue-gallery';
+
+  export default {
+    data: function () {
+      return {
+        images: [
+          'https://mypeers-api.herokuapp.com/images/prod/1531684362_5b4ba60a26d42.jpeg'
+
+        ],
+        index: null
+      };
+    },
+
+    components: {
+      'gallery': VueGallery
+    },
+  }
+</script>
--- src/pages/teste.vue
+++ src/pages/teste.vue
@@ -1,19 +1,72 @@
 <template>
-  <div>
-  </div>
+  <q-page padding class="text-center">
+      <vue-galeria>
+      </vue-galeria>
+      <section class="content-parallax" style="width:97%;">
+        <section class="titulo">
+          <h1> {{ this.getProduto.nome }}</h1>
+        </section>
+        <section class="preco">
+          <p> R$ {{ this.getProduto.preco }} </p>
+        </section>
+        <div style="word-wrap: break-word;">
+         <p class="caption"> {{ this.getProduto.descricao }} </p>
+        </div>
+
+        <section class="info-loja">
+          <modal/>
+        </section>
+      </section>
+  </q-page>
 </template>
 
 <script>
-  export default {
-    name:'',
-    data(){
-       return {
+import Modal from '../components/Modal';
+import Parallax from 'vue-parallaxy'
+import vueGaleria from '../components/vueGaleria';
+import { mapGetters } from 'vuex';
 
-      }
-    },
+export default {
+  name: 'PaginaDetalheCalcado',
+  components: {
+    modal: Modal,
+    'parallax' : Parallax,
+    'vue-galeria': vueGaleria,
+  },
+  computed: {
+    ...mapGetters({
+      getProduto: 'getProduto'
+    }),
+
   }
+}
 </script>
 
-<style>
+<style scoped>
 
+  .content-parallax {
+    background-color: #fff;
+    position:absolute;
+    margin-top: -8rem;
+    width:100%;
+    padding: 0.5rem;
+  }
+  .preco{
+    font-size: 1.2rem;
+    color: gray;
+  }
+  .img-parallax {
+    width: 100%;
+    margin-top:3rem;
+  }
+
+  .descricao{
+  }
+  .titulo h1{
+    font-size: 1.5rem;
+    color:#027be3;
+  }
+  .info-loja {
+
+  }
 </style>
